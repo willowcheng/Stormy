590afacb7618e064226f4a727ea2bcd85a483894
willowcheng <e.wingus@Gmail.com>
2015-05-23 19:41:37 -0400

    Networking in ios

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let apiKey = "fb9bc4b82191b64d13b661b6897f06ea"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+        let forecastURL = NSURL(string: "37.8267,-122.423", relativeToURL: baseURL)
     }
 
     override func didReceiveMemoryWarning() {
