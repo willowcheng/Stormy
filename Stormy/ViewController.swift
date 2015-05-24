b16bc84565b81e7b58e9fc08fc7ce8943436cdca
willowcheng <e.wingus@Gmail.com>
2015-05-23 20:49:18 -0400

    Using JSON data

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -20,7 +20,13 @@ class ViewController: UIViewController {
         
         let sharedSession = NSURLSession.sharedSession()
         let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(forecastURL!, completionHandler: { (location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
-            println(response)
+            
+            if error == nil {
+                let dataObject = NSData(contentsOfURL: location)
+                let weatherDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject!, options: nil, error: nil) as! NSDictionary
+                println(weatherDictionary)
+            }
+            
         })
         downloadTask.resume()
     }
